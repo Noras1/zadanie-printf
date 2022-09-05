4fc99a99511d5bccd29ebcc4e68b22bf5de09876
Noras1 <62107063+Noras1@users.noreply.github.com>
2022-09-05 10:48:28 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <conio.h>
+#include <stdlib.h>
+int main()
+{
+  
+  char a;
+  printf("1. Liczba zmiennoprzecinkowa: \n");
+  printf("2. Liczba w systemie 16: \n");
+  printf("3. zera z przodu: \n");
+ printf("4. wyjscie\n");
+  char key = getch();
+    switch (key){
+    ;
+    printf("string %i", a);
+
+scanf("%i", a);
+    case '1':
+    	
+	printf( "%.2f \n", 2.345);
+	break;
+   case '2':
+   	
+    printf( "%x  \n",  255);
+    break;
+    case '3':
+    
+	printf( "%08d \n", 12);
+	break;
+}
+
+    
+	return 0;
+}
